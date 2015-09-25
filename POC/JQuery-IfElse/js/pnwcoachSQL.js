d0dcf1e8d9cb4db1be80d9ded4f3c3e7ce1adb6f
Bruce Perry <bruce@iperry.net>
2015-09-24 23:28:42 -0700

    Cleanup Activity

---
+++ POC/JQuery-IfElse/js/pnwcoachSQL.js
@@ -0,0 +1,21 @@
+$.ajax(
+        {url: 'http://monkey-problems.com/PNWCoachesSQL/apps/mySQLiArray.php',
+        data: {action: 'test'},
+        dataType: 'json',
+        type: 'post',
+         
+          success: function(output) {
+            var $name = output[0];
+            var $city = output[1];
+            var $story = output[2];
+            var $statement = output[3];
+            var $pic = output[4];
+            var $status = output[5];
+
+            document.getElementById("coachPhoto").src = $pic;   
+            document.getElementById("coachName").innerHTML = $name;   
+            document.getElementById("coachStory").innerHTML = $story;   
+            document.getElementById("coachStatement").innerHTML = $statement;   
+          }
+        }
+);
